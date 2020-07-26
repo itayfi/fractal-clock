57435fba0ed1e573d77fb6b196b4a8ab146a7d83
Itay Fintz <itayfi@gmail.com>
2020-07-26 19:50:07 +0300

    Updates

---
+++ precache-manifest.aa97e1bb572032030563f39d99848f79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd85dc2dfcaccdb88e0ba9355675db8",
+    "url": "/fractal-clock/index.html"
+  },
+  {
+    "revision": "9e98d1545e749a438844",
+    "url": "/fractal-clock/static/css/main.1c393315.chunk.css"
+  },
+  {
+    "revision": "f1b27f3537e35969a6cf",
+    "url": "/fractal-clock/static/js/2.9a31eb09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fractal-clock/static/js/2.9a31eb09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e98d1545e749a438844",
+    "url": "/fractal-clock/static/js/main.a331a83e.chunk.js"
+  },
+  {
+    "revision": "6c65547d0ace86d4e09c",
+    "url": "/fractal-clock/static/js/runtime-main.8402a5c5.js"
+  }
+]);
